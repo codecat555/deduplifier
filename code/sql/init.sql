dbd1e8af92e43a2e31b2f65c49e3e713e7056a68
me <codemonkey555@gmail.com>
2020-11-19 11:58:40 -0800

    db - added date fields to file table.

--- code/sql/init.sql
+++ code/sql/init.sql
@@ -12,6 +12,10 @@ CREATE TABLE file (
     id INT GENERATED ALWAYS AS IDENTITY,
     location_id INT NOT NULL,
     name VARCHAR(1024) NOT NULL,
+    create_date DATE NOT NULL,
+    modify_date DATE NOT NULL,
+    access_date DATE NOT NULL,
+    discover_date DATE NOT NULL,
     PRIMARY KEY(id),
     CONSTRAINT fk_location FOREIGN KEY(location_id) REFERENCES location(id)
 );
