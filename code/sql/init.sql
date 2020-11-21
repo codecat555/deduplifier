94ddecb7f321158c96f29d12debc15b89bcfdbe3
me <codemonkey555@gmail.com>
2020-11-20 22:14:40 -0800

    Working on adding new-db make file target. Also, expanding the db structure.

--- code/sql/init.sql
+++ code/sql/init.sql
@@ -1,13 +1,53 @@
 
-DROP TABLE IF EXISTS Location CASCADE;
-CREATE TABLE location (
+DROP DATABASE IF EXISTS deduplifier;
+CREATE DATABASE deduplifier;
+CONNECT TO deduplifier;
+
+--DROP TABLE IF EXISTS host CASCADE;
+CREATE TABLE host (
     id INT GENERATED ALWAYS AS IDENTITY,
-    host VARCHAR(253) NOT NULL,
-    path VARCHAR(1024) NOT NULL,
+    name VARCHAR(253) NOT NULL,
     PRIMARY KEY(id)
 );
 
-DROP TABLE IF EXISTS file CASCADE;
+--DROP TABLE IF EXISTS drive CASCADE;
+CREATE TABLE drive (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    serialno VARCHAR(64) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+--DROP TABLE IF EXISTS volume CASCADE;
+CREATE TABLE volume (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    uuid VARCHAR(40) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+--DROP TABLE IF EXISTS path CASCADE;
+CREATE TABLE path (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    parent_path_id INT,
+    path VARCHAR(40) NOT NULL,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_parent_path FOREIGN KEY(parent_path_id) REFERENCES path(id)
+);
+
+--DROP TABLE IF EXISTS location CASCADE;
+CREATE TABLE location (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    host_id INT NOT NULL,
+    drive_id INT NOT NULL,
+    volume_id INT NOT NULL,
+    path_id INT NOT NULL,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_host FOREIGN KEY(host_id) REFERENCES host(id),
+    CONSTRAINT fk_drive FOREIGN KEY(drive_id) REFERENCES drive(id),
+    CONSTRAINT fk_volume FOREIGN KEY(volume_id) REFERENCES volume(id),
+    CONSTRAINT fk_path FOREIGN KEY(path_id) REFERENCES path(id)
+);
+
+--DROP TABLE IF EXISTS file CASCADE;
 CREATE TABLE file (
     id INT GENERATED ALWAYS AS IDENTITY,
     location_id INT NOT NULL,
@@ -20,12 +60,18 @@ CREATE TABLE file (
     CONSTRAINT fk_location FOREIGN KEY(location_id) REFERENCES location(id)
 );
 
-DROP TABLE IF EXISTS image_file CASCADE;
+--DROP TABLE IF EXISTS image_file CASCADE;
 CREATE TABLE image_file (
-    id INT GENERATED ALWAYS AS IDENTITY,
     file_id INT NOT NULL,
-    name VARCHAR(1024) NOT NULL,
-    imagehash_fingerprint VARCHAR(1024),
-    PRIMARY KEY(id),
+    image_id INT NOT NULL,
     CONSTRAINT fk_file FOREIGN KEY(file_id) REFERENCES file(id)
+    CONSTRAINT fk_image FOREIGN KEY(image_id) REFERENCES image(id)
+);
+
+--DROP TABLE IF EXISTS image CASCADE;
+CREATE TABLE image (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY
+    name VARCHAR(1024) NOT NULL,
+    imagehash_fingerprint VARCHAR(1024)
+
 );
