f87f1422190da8130de288c6f7f05f8fb0c90fc1
codecat555 <codemonkey555@gmail.com>
2022-04-03 22:56:24 -0700

    Update duplicate files web interface.

--- code/web/app.py
+++ code/web/app.py
@@ -71,7 +71,6 @@ class app_db:
         try:
             with self.conn.cursor() as cursor:
                 cursor.callproc('get_totals', [])
-                # result = cursor.fetchone()
                 result = cursor.fetchall()[0]
                 cursor.close()
         finally:
@@ -87,7 +86,6 @@ class app_db:
         try:
             with self.conn.cursor() as cursor:
                 cursor.callproc('get_counts', [])
-                # result = cursor.fetchall()
                 result = cursor.fetchall()[0]
                 cursor.close()
         finally:
@@ -137,22 +135,30 @@ def welcome():
     #return 'Hello World! I have been seen {} times.\n'.format(count)
     return db.welcome()
 
+
 @app.route('/files_with_dups', defaults={ 'start_row': 1, 'rows_per_page': 20 }, methods=['GET', 'POST'])
-# @app.route('/files_with_dups/<int:start_idx>/<int:rows_per_page>')
 def files_with_dups(start_row, rows_per_page):
-    db = app_db(connection_parameters)
-    if request.method == 'POST':
-        start_row = int(request.form['start_row'])
-        rows_per_page = int(request.form['rows_per_page'])
-        if 'pager' in request.form:
-            if request.form['pager'] == 'previous':
-                start_row = max(start_row-rows_per_page, 0)
-            elif request.form['pager'] == 'next':
-                start_row += rows_per_page
-            else:
-                raise BadRequest(str(request))
+
+    headers = ['#', 'Duplicates', 'Total Mb', 'File Name']
+
+    result = None
+    testing = False
+    if testing:
+        result = [[7, 10661405447, 'Teeth Main Assem - Lower Gum-2 Mirror3[13]-1.STL'], [7, 10660059109, 'Teeth Main Assem - Lower Gum-2 Imported14-1.STL'], [7, 10571394225, 'Teeth Main Assem - Lower Gum-2 Lower Gum-1.STL'], [7, 10256363012, 'Teeth Main Assem - Lower Gum-2 Imported18-1.STL'], [7, 10255755419, 'Teeth Main Assem - Lower Gum-2 Mirror3[1]-1.STL'], [7, 10200022665, 'Teeth Main Assem - Lower Gum-2 Imported2-1.STL'], [7, 10014125380, 'Planmeca_Imaging_Package_2018-01.zip'], [53, 9997525203, 'Teeth Main Assem.STEP'], [5, 8855311995, 'genTB.log.old'], [6, 8367710208, '6.0.6001.18000.367-KRMSDK_EN.iso'], [12, 6996737532, 'MFC group picture - 1989.psd'], [7, 6598810624, 'alt-outlook.pst'], [23, 6427305472, 'NONAME_001.AVI'], [12, 6411512220, 'MFC group picture - 1990.psd'], [7, 5522404020, 'Teeth Main Assem - Lower Gum-2 Mirror3[10]-1.STL'], [7, 5520882780, 'Teeth Main Assem - Lower Gum-2 Imported10-1.STL'], [8, 5481625992, 'Mandible_FMA52748_2mm.stp'], [3, 4815689013, '014.mp4'], [5, 4713436160, 'outlook.pst'], [15, 4564638720, 'outlook.pst']]
+    else:
+        db = app_db(connection_parameters)
+        if request.method == 'POST':
+            start_row = int(request.form['start_row'])
+            rows_per_page = int(request.form['rows_per_page'])
+            if 'pager' in request.form:
+                if request.form['pager'] == 'previous':
+                    start_row = max(start_row-rows_per_page, 0)
+                elif request.form['pager'] == 'next':
+                    start_row += rows_per_page
+                else:
+                    raise BadRequest(str(request))
         
-    result = db.list_files_with_dups(start_row-1, rows_per_page)
+        result = db.list_files_with_dups(start_row-1, rows_per_page)
 
     previous_page_status = ''
     if start_row == 1:
@@ -171,12 +177,14 @@ def files_with_dups(start_row, rows_per_page):
     return render_template(
             'list_files_with_dups.html',
             title='Duplicate Files',
-            description='List of Known Files with counts of duplicates',
+            description='Listing of files having one or more duplicates',
             start_row=start_row,
             rows_per_page=rows_per_page,
             row_count=len(result),
+            post_url='/files_with_dups',
             next_page_status=next_page_status, 
             previous_page_status=previous_page_status, 
+            headers=headers,
             files=result
     )
 
